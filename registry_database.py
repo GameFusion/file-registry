5417bdba15088b8854fd98ee81bb970ea6c304cc
Andreas Carlen <andreas@gamefusion.biz>
2023-12-22 20:25:36 +0100

    new display log command : file_registry_log.py

---
+++ registry_database.py
@@ -0,0 +1,30 @@
+
+
+import mysql.connector
+import json
+
+def get_database_connection():
+    # Load the credentials from the JSON file
+    with open('credentials.json') as f:
+        credentials = json.load(f)
+
+    # Connect to the MySQL database
+    try:
+        cnx = mysql.connector.connect(
+            user=credentials['user'],
+            password=credentials['password'],
+            host=credentials['host'],
+            database=credentials['database']
+        )
+        return cnx
+    except mysql.connector.Error as err:
+        print(f"Error connecting to the database: {err}")
+        return None
+
+def is_connection_valid(cnx):
+    try:
+        # Check if connection is still alive
+        return cnx.is_connected()
+    except:
+        # If an error occurs, assume the connection is not valid
+        return False
